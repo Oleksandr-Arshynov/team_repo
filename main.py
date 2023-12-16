40b854a917baa6571c59ac5e50ecc6798355e24e
Oleksandr Arshynov <123052798+Oleksandr-Arshynov@users.noreply.github.com>
2023-12-16 18:01:24 +0200

    add_table

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from tabulate import tabulate
 from rich.table import Table
 from rich.text import Text
 from dateutil import parser
+from rich.live import Live
+import sys
 
 console = Console()
 
@@ -29,7 +31,7 @@ class PersonalAssistant:
         self.notes = []
         self.commands = ['додати контакт', 'список контактів', 'пошук контактів', 'дні народження', 
                          'додати нотатку', 'пошук нотаток', 'видалити нотатку', 'список нотаток', 
-                         'редагувати нотатку', 'сортувати нотатки']
+                         'редагувати нотатку', 'сортувати нотатки', 'допомога']
 
         # Встановлення автодоповнення на основі доступних команд
         self.command_completer = WordCompleter(self.commands)
@@ -118,34 +120,36 @@ class PersonalAssistant:
                     Text(birthday_str, style="magenta")
                 )
 
-            console.print(table)
+            # Встановлення відстані від верхнього краю екрану
+            console.print("\n" * 10)
+            console.print(table, justify="center")
 
 
     def upcoming_birthdays(self, days):
         today = datetime.today().date()
         upcoming_birthdays = [contact for contact in self.contacts if today < self.get_next_birthday(contact) <= today + timedelta(days)]
         if not upcoming_birthdays:
-            console.print(f'[red]Немає наближених днів народження протягом {days} днів.[/red]')
+            console.print(f'[yellow]У {days} днів немає найближчих днів народження.[/yellow]')
         else:
-            table = Table(title=f'Наближені дні народження протягом {days} днів')
+            table = Table(title=f'Дні народження у наступні {days} днів')
             table.add_column("[blue]Ім'я[/blue]")
-            table.add_column("[magenta]День народження[/magenta]")
-            table.add_column("[green]Залишилось днів[/green]")
-            table.add_column("[cyan]Вік[/cyan]")
+            table.add_column("[magenta]Дата народження[/magenta]")
+            table.add_column("[yellow]Залишилося днів[/yellow]")
 
             for contact in upcoming_birthdays:
                 remaining_days = (self.get_next_birthday(contact) - today).days
-                age = today.year - contact['birthday'].year - ((today.month, today.day) < (contact['birthday'].month, contact['birthday'].day))
+                birthday_str = contact['birthday'].strftime('%d-%m-%Y')
+
                 table.add_row(
                     Text(contact['name'], style="blue"),
-                    Text(self.get_next_birthday(contact).strftime('%d.%m.%Y'), style="magenta"),
-                    Text(str(remaining_days), style="green"),
-                    Text(str(age), style="cyan")
+                    Text(birthday_str, style="magenta"),
+                    Text(str(remaining_days), style="yellow")
                 )
-
-            console.print(table)
-   
-
+            # Встановлення відстані від верхнього краю екрану
+            console.print("\n" * 10)
+            console.print(table, justify="center")
+            
+            
     def get_next_birthday(self, contact):
         today = datetime.today().date()
 
@@ -167,30 +171,50 @@ class PersonalAssistant:
             query = input("Введіть запит для пошуку контактів: ")
 
         matching_contacts = [contact for contact in self.contacts if query.lower() in contact['name'].lower()]
-        
+
         if matching_contacts:
             console.print(f"[bold green]Результати пошуку:[/bold green]")
+
+            # Виведення знайдених контактів в таблицю
+            table = Table(title="Знайдені контакти")
+            table.add_column("[blue]Ім'я[/blue]", justify="center")
+            table.add_column("[green]Адреса[/green]", justify="center")
+            table.add_column("[yellow]Телефон[/yellow]", justify="center")
+            table.add_column("[cyan]Електронна пошта[/cyan]", justify="center")
+            table.add_column("[magenta]День народження[/magenta]", justify="center")
+
             for contact in matching_contacts:
-                console.print(contact)
+                table.add_row(
+                    Text(contact['name'], style="blue"),
+                    Text(contact['address'], style="green"),
+                    Text(contact['phone'], style="yellow"),
+                    Text(contact['email'], style="cyan"),
+                    Text(contact['birthday'].strftime('%d-%m-%Y'), style="magenta")
+                )
+
+            # Центрування таблиці
+            console.print(table, justify="center")
         else:
             console.print(f"[red]Немає результатів пошуку для запиту: {query}[/red]")
-
+        
         while True:
             user_input = input("Введіть команду: ")
-            assistant.analyze_user_input(user_input)
+            self.analyze_user_input(user_input)
 
             # Перевірка команд і виклики відповідних методів
             if "додати контакт" in user_input.lower():
-                assistant.add_contact_from_console()
+                self.add_contact_from_console()
             elif "список контактів" in user_input.lower():
-                assistant.list_contacts()
+                self.list_contacts()
             elif "пошук контактів" in user_input.lower():
-                assistant.search_contacts()
+                self.search_contacts()
             elif "дн" in user_input.lower():
-                assistant.upcoming_birthdays(7)
+                self.upcoming_birthdays(7)
+            elif "допомога" in user_input.lower(): 
+                self.display_commands_table()
             else:
                 console.print("[red]Не можу розпізнати вашу команду. Спробуйте ще раз.[/red]")
-
+                
     def edit_contact(self, contact_index, name, address, phone, email, birthday):
         # Редагування контакту
         pass
@@ -209,29 +233,33 @@ class PersonalAssistant:
         console.print("[bold]Додавання нових нотаток:[/bold]")
 
         while True:
-            text = input("Текст нотатки (або введіть 'закінчити' для завершення): ")
-            if text.lower() == 'закінчити':
+            text = input("Текст нотатки (або введіть [/red]'закінчити'[/red] чи  [/red]'вийти'[/red] для завершення): ")
+            if text.lower() == 'закінчити' or text.lower() == 'вийти':
                 break
 
             tags = input("Теги (розділіть їх комою): ").split(',')
             self.add_note(text, tags)
 
     def list_notes(self):
-        # Виведення списку нотаток
+            # Виведення списку нотаток
         if not self.notes:
             console.print("[red]У вас немає жодних нотаток.[/red]")
-        else:
-            table = Table(title="Список нотаток")
-            table.add_column("[blue]Текст[/blue]")
-            table.add_column("[cyan]Теги[/cyan]")
+            return  # Повернення з функції, оскільки немає нотаток для редагування
 
-            for note in self.notes:
-                table.add_row(
-                    Text(note.text, style="blue"),
-                    Text(", ".join(note.tags), style="cyan")
-                )
+        table = Table(title="Список нотаток")
+        table.add_column("[blue]Номер[/blue]")
+        table.add_column("[blue]Текст[/blue]")
+        table.add_column("[cyan]Теги[/cyan]")
 
-            console.print(table)
+        for i, note in enumerate(self.notes):
+            table.add_row(
+                Text(str(i), style="blue"),
+                Text(note.text, style="blue"),
+                Text(", ".join(note.tags), style="cyan")
+            )
+
+        console.print(table, justify="center")
+        console.print(f"[green]Кількість існуючих нотаток: {len(self.notes)}[/green]")
 
     def search_notes(self, query):
         # Пошук нотаток за запитом
@@ -244,9 +272,24 @@ class PersonalAssistant:
         else:
             console.print(f"[red]Немає результатів пошуку для запиту: {query}[/red]")
 
-    def edit_note(self, note_index, text, tags):
-        # Редагування нотатки
-        pass
+
+    def edit_note(self, note_index):
+        if 0 <= note_index < len(self.notes):
+            # Отримання нотатки за індексом
+            note_to_edit = self.notes[note_index]
+
+            # Редагування тексту нотатки
+            new_text = input("Введіть новий текст нотатки: ")
+            note_to_edit.text = new_text
+
+            # Редагування тегів нотатки
+            new_tags = input("Введіть нові теги нотатки (через кому): ")
+            note_to_edit.tags = [tag.strip() for tag in new_tags.split(',')]
+
+            console.print(f"[green]Нотатка {note_index} успішно відредагована.[/green]")
+        else:
+            console.print("[red]Невірний індекс нотатки. Спробуйте ще раз.[/red]")
+
 
     def delete_note(self, note_index):
         # Видалення нотатки
@@ -263,54 +306,96 @@ class PersonalAssistant:
     def analyze_user_input(self, user_input):
         normalized_input = user_input.lower()
         if "додати контакт" in normalized_input:
-            console.print("[green]Пропоную вам додати новий контакт. Використайте команду add_contact.[/green]")
+            console.print("[green]Пропоную вам додати новий контакт. Використайте команду додати контакт.[/green]")
         elif "список контактів" in normalized_input:
-            console.print("[green]Для виведення списку контактів використайте команду list_contacts.[/green]")
+            console.print("[green]Для виведення списку контактів використайте команду список контактів.[/green]")
         elif "пошук контактів" in normalized_input:
-            console.print("[green]Ви можете використовувати команду search_contacts для пошуку контактів.[/green]")
-        elif "дн" in normalized_input:
-            console.print("[green]Ви можете використовувати команду search_contacts для пошуку контактів.[/green]")
+            console.print("[green]Ви можете використовувати команду пошук контактів для пошуку контактів.[/green]")
+        elif "дні народження" in normalized_input:
+            console.print("[green]Ви можете використовувати команду дні народження для пошуку контактів у яких буде день народження.[/green]")
         elif "додати нотатку" in normalized_input:
-            console.print("[green]Ви можете використовувати команду search_contacts для пошуку контактів.[/green]")
+            console.print("[green]Ви можете використовувати команду додати нотатку для додавання нотаток.[/green]")
         elif "список нотаток" in normalized_input:
-            console.print("[green]Ви можете використовувати команду search_contacts для пошуку контактів.[/green]")
+            console.print("[green]Для виведення списку нотаток використайте команду список нотаток.[/green]")
         elif "редагувати нотатку" in normalized_input:
-            console.print("[green]Ви можете використовувати команду search_contacts для пошуку контактів.[/green]")
+            console.print("[green]Для редагування нотатки використайте команду редагувати нотатку.[/green]")
         elif "сортувати нотатки" in normalized_input:
-            console.print("[green]Ви можете використовувати команду search_contacts для пошуку контактів.[/green]")   
+            console.print("[green]Для сортування нотаток використайте команду сортувати нотатки.[/green]")   
         else:
             console.print("[red]Не можу розпізнати вашу команду. Спробуйте ще раз.[/red]")
+            
+    def display_commands_table(self):
+        """Створює таблицю зі списком доступних команд і виводить її в консолі"""
+        # Створення об'єкта Console
+        console = Console()
 
-assistant = PersonalAssistant()
+        # Створення таблиці зі списком команд
+        table = Table(title="Доступні команди")
+        table.add_column("[cyan]Команда[/cyan]", justify="center")
 
-while True:
-    user_input = input("Введіть команду: ")
-    assistant.analyze_user_input(user_input)
+        for i, command in enumerate(self.commands):
+            # Визначення кольору для кожного рядка
+            color = "red" if i % 2 == 0 else "cyan"
 
-    # Перевірка команд і виклик відповідного методу
-    if "додати контакт" in user_input.lower():
-        assistant.add_contact_from_console()
-    elif "список контактів" in user_input.lower():
-        assistant.list_contacts()
-    elif "пошук контактів" in user_input.lower():
-        assistant.search_contacts()
-    elif "дні народження" in user_input.lower():
-        assistant.upcoming_birthdays(7)
-    elif "пошук нотаток" in user_input.lower():
-        query = input("Введіть запит для пошуку нотаток: ")
-        assistant.search_notes(query)
-    elif "додати нотатку" in user_input.lower():
-        assistant.add_notes_from_console()
-    elif "видалити нотатку" in user_input.lower():
-        assistant.delete_note_note()
-    elif "список нотаток" in user_input.lower():
-        assistant.list_notes()    
-    elif "редагувати нотатку" in user_input.lower():
-        assistant.edit_note()
-    elif "сортувати нотатки" in user_input.lower():
-        assistant.sort_notes_by_tags() 
-      
-    else:
-        console.print("[red]Не можу розпізнати вашу команду. Спробуйте ще раз.[/red]") 
-    #додати нотатку', 'пошук нотаток', 'видалити нотатку', 'список нотаток', 
-    #'редагувати нотатку', 'сортувати нотатки'
+            # Додавання рядка з визначеним кольором
+            table.add_row(f"[{color}]{command}[/{color}]")
+
+        # Виведення таблиці в консоль з використанням Live
+        with Live(refresh_per_second=1, console=console) as live:
+            console.print(table, justify="center")  # Виведення таблиці з вирівнюванням по центру
+            input("Натисніть Enter для завершення перегляду команд...")  # Очікування вводу від користувача
+            live.stop()
+
+
+    def run(self):
+        """ Основний цикл виконання програми. Полягає в тому, 
+            що він виводить вітання та список команд, а потім 
+            чекає на введення команди"""
+        console = Console()
+        console.print(
+        "\n[bold yellow]Вітаю, я ваш особистий помічник![/bold yellow]\n",
+        justify="center",
+        style="bold",
+        width=200,
+        )
+        
+        self.display_commands_table()
+        
+        while True:
+            user_input = input("Введіть команду: ")
+            assistant.analyze_user_input(user_input)
+            # Перевірка команд і виклик відповідного методу
+            
+            if "допомога" in user_input.lower() :
+                assistant.display_commands_table()
+            elif "додати контакт" in user_input.lower():
+                assistant.add_contact_from_console()
+            elif "список контактів" in user_input.lower():
+                assistant.list_contacts()
+            elif "пошук контактів" in user_input.lower():
+                assistant.search_contacts()
+            elif "дні народження" in user_input.lower():
+                assistant.upcoming_birthdays(7)
+            elif "пошук нотаток" in user_input.lower():
+                query = input("Введіть запит для пошуку нотаток: ")
+                assistant.search_notes(query)
+            elif "додати нотатку" in user_input.lower():
+                assistant.add_notes_from_console()
+            elif "видалити нотатку" in user_input.lower():
+                assistant.delete_note_note()
+            elif "список нотаток" in user_input.lower():
+                assistant.list_notes()    
+            elif "редагувати нотатку" in user_input.lower():
+                if "редагувати нотатку" in user_input.lower():
+                    note_index = int(input("Введіть номер нотатки, яку ви хочете відредагувати: "))
+                    assistant.edit_note(note_index)
+            elif "сортувати нотатки" in user_input.lower():
+                assistant.sort_notes_by_tags() 
+            
+            else:
+                console.print("[red]Не можу розпізнати вашу команду. Спробуйте ще раз.[/red]") 
+
+
+if __name__ == "__main__":
+    assistant = PersonalAssistant()
+    assistant.run()
