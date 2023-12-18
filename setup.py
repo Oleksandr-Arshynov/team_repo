5d169bdc96a57e5a2afab94da2f97fd310699616
Oleksandr Arshynov <123052798+Oleksandr-Arshynov@users.noreply.github.com>
2023-12-18 19:45:52 +0200

    as

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='assistant',
     version='0.1',
     packages=find_packages(),
+    description='Assistant packege',
+    long_description='Your long description here.',
+    long_description_content_type='text/x-rst'
     install_requires=[
         'rich',
         'prompt_toolkit',
