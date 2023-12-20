00ae2b09103da6f28f8c329dcdf465b9e2563146
Oleksandr Arshynov <123052798+Oleksandr-Arshynov@users.noreply.github.com>
2023-12-20 20:14:04 +0200

    done

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name='assistant',
     version='0.1',
     packages=find_packages(),
-    description='Assistant packege',
+    description='Assistant package',
     long_description='Your long description here.',
-    long_description_content_type='text/x-rst'
+    long_description_content_type='text/x-rst',
     install_requires=[
         'rich',
         'prompt_toolkit',
-        'tabulate',
+        'python-dateutil',
     ],
 )
